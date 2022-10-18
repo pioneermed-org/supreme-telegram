1f9a56af04995ce3cac835fee6207abfbb6f6b70
dej000 <dondejavul@yahoo.com>
2022-10-18 17:38:43 +0100

    new-branch

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import Chakra, { CThemeProvider,CReset, CColorModeProvider,
+//   CBox} from '@chakra-ui/vue'
+import "bootstrap/dist/css/bootstrap.css";
+import './index.css'
 
-import './assets/main.css'
 
 const app = createApp(App)
+import "bootstrap/dist/js/bootstrap.js";
 
 app.use(router)
 
 app.mount('#app')
+// Vue.use(Chakra)
+
+// new Vue({
+//   render(h) {
+   
+//     return h(CThemeProvider, [
+//       h(CColorModeProvider, [h(CBox, [h(CReset), h(App)])])
+//     ]);
+//   }
+// }).$mount("#app");
+
